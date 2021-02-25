dde5a9ca50918dfc509a4ba816f19e8b004f2b71
Wojtek <wojtas5127>
2021-02-25 11:12:58 +0100

    zadania

---
+++ main.py
@@ -0,0 +1,57 @@
+from math import *
+
+#zad 1
+# a = 'napis'
+# b = 'napis2'
+# c = 3
+# d = 7
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+#zad 2
+
+# a = 3
+# b = 9
+# c = 20
+# d = 4
+#
+# print(c+b)
+# print(c-b)
+# print(a*b)
+# print(c/d)
+
+#zad 3
+
+# a = 2
+# b = 4
+# c = 20
+# d = 5
+# e = 15
+# f = 5
+#
+# a += 2
+# b *= 3
+# c /= 4
+# d -= 3
+# e %= 4
+# f **= 3
+#
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+#zad 4
+
+e = exp(1)
+e**=10
+
+p = int(pow(log(5+sin(8)**2),1/6))
+
+print(e)
+print(p)
